4dde4a95a3052d59edb7808d5e558275670cc394
starriver <starriver820@gmail.com>
2019-09-28 00:18:12 +0800

    [Add] commit reverse string codes

---
+++ ReverseString/index.js
@@ -0,0 +1,27 @@
+/*
+* Directions:
+* Return a string with order of characters reversed
+* Example:
+* 'abcd' ===> 'dcba'
+*/
+function reverseString(str){
+    if(typeof str !== 'string'){
+        return false
+    }
+    return str.split("").reduce((frontStr,currentStr)=>{
+        frontStr = currentStr+frontStr
+        return frontStr
+    },"")
+}
+
+mocha.setup('bdd');
+const { assert } = chai
+
+describe('String Reversal', ()=>{
+    it('reverse() correctly reverses string', ()=>{
+        assert.equal(reverseString('ffaa'), 'aaff')
+        assert.equal(reverseString('  aaff'), 'ffaa  ')
+    })
+})
+
+mocha.run();
